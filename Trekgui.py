591ac46aa8a5e02651a6b19f7df4d43f0ccc6f5c
Chris Samuelson <chris.sam55@gmail.com>
2017-02-12 12:50:54 -0500

    Removed braces from button text

--- Trekgui.py
+++ Trekgui.py
@@ -19,19 +19,19 @@ class Application(tk.Frame):
     def create_Manufacture(self):
         global manufacture
         self.btn_manufacture = tk.Button(self)
-        self.btn_manufacture["text"] = "{Manufacture = } 0"
+        self.btn_manufacture["text"] = "Manufacture = 0"
         self.btn_manufacture["command"] = self.add_Manufacture
         self.btn_manufacture.pack(side="top")
                     
     def create_Research(self):
         self.btn_research = tk.Button(self)
-        self.btn_research["text"] = "{Research = } 0"
+        self.btn_research["text"] = "Research = 0"
         self.btn_research["command"] = self.add_Research
         self.btn_research.pack(side="top")
 
     def create_Culture(self):
         self.btn_culture = tk.Button(self)
-        self.btn_culture["text"] = "{Culture =} 0"
+        self.btn_culture["text"] = "Culture = 0"
         self.btn_culture["command"] = self.add_Culture
         self.btn_culture.pack(side="top")    
     
@@ -39,23 +39,23 @@ class Application(tk.Frame):
     def add_Manufacture(self):
         global manufacture
         manufacture += 1
-        print("Manufacture = ", manufacture)
-        manufacture_Button = 'Manufacture = ', manufacture
-        self.btn_manufacture.configure(text=manufacture_Button)
+        string = "Manufacture = " + str(manufacture)
+        print(string)
+        self.btn_manufacture["text"] = string
         
     def add_Research(self):
         global research
         research += 1
-        print("Research = ", research)
-        research_Button = 'Research = ', research
-        self.btn_research.configure(text=research_Button)
+        string = "Research = " + str(research)
+        print(string)
+        self.btn_research["text"] = string
         
     def add_Culture(self):
         global culture
         culture += 1
-        print("Culture = ", culture)
-        culture_Button = 'Culture = ', culture
-        self.btn_culture.configure(text=culture_Button)
+        string = "Culture = " + str(culture)
+        print(string)
+        self.btn_culture["text"] = string
 
 if __name__ == "__main__":
     root = tk.Tk()
