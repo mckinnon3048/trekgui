b0a6933642ef2b083d0c5a243d1c953192ef9afa
Chris Samuelson <chris.sam55@gmail.com>
2017-02-12 11:34:32 -0500

    Improved clarity of execution entry point

--- Trekgui.py
+++ Trekgui.py
@@ -57,6 +57,8 @@ class Application(tk.Frame):
         culture_Button = 'Culture = ', culture
         self.btn_culture.configure(text=culture_Button)
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
+
