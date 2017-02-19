bc9f2f64d08c55ba8b7fe7b9e5884ae0632b9edd
Chris Samuelson <chris.sam55@gmail.com>
2017-02-19 17:09:29 -0500

    Removed code redundancy

--- Trekgui.py
+++ Trekgui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import tkinter as tk
+from functools import partial
 
 manufacture = 0
 research = 0
@@ -11,51 +12,23 @@ class Application(tk.Frame):
         super().__init__(master)
         master.minsize(width=250, height=100)
         self.pack(side="top")
-        self.create_Manufacture()
-        self.create_Research()
-        self.create_Culture()
+        self.buttons = {}
+        self.resources = {}
+        self.create_button("Manufacture")
+        self.create_button("Research")
+        self.create_button("Culture")
+        self.create_button("Ascendency tokens")
 
+    def add_resource(self, resource):
+        self.resources[resource] += 1
+        self.buttons[resource]["text"] = resource + " = " + str(self.resources[resource])
 
-    def create_Manufacture(self):
-        global manufacture
-        self.btn_manufacture = tk.Button(self)
-        self.btn_manufacture["text"] = "Manufacture = 0"
-        self.btn_manufacture["command"] = self.add_Manufacture
-        self.btn_manufacture.pack(side="top")
-                    
-    def create_Research(self):
-        self.btn_research = tk.Button(self)
-        self.btn_research["text"] = "Research = 0"
-        self.btn_research["command"] = self.add_Research
-        self.btn_research.pack(side="top")
-
-    def create_Culture(self):
-        self.btn_culture = tk.Button(self)
-        self.btn_culture["text"] = "Culture = 0"
-        self.btn_culture["command"] = self.add_Culture
-        self.btn_culture.pack(side="top")    
-    
-
-    def add_Manufacture(self):
-        global manufacture
-        manufacture += 1
-        string = "Manufacture = " + str(manufacture)
-        print(string)
-        self.btn_manufacture["text"] = string
-        
-    def add_Research(self):
-        global research
-        research += 1
-        string = "Research = " + str(research)
-        print(string)
-        self.btn_research["text"] = string
-        
-    def add_Culture(self):
-        global culture
-        culture += 1
-        string = "Culture = " + str(culture)
-        print(string)
-        self.btn_culture["text"] = string
+    def create_button(self, name):
+        self.resources[name] = 0
+        self.buttons[name] = tk.Button(self)
+        self.buttons[name]["text"] = name + " = 0"
+        self.buttons[name]["command"] = partial(self.add_resource, name)
+        self.buttons[name].pack(side="top")
 
 if __name__ == "__main__":
     root = tk.Tk()
