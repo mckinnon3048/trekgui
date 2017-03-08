ddb9c08ffc1eff26e05ad2cfc4e7ae984acf55dc
mckinnon3048 <mckinnon3048@gmail.com>
2017-03-07 20:50:11 -0500

    Phases, minor documentation
    Added menu to select phase of game, documented some intent and broken
    functions, **only phase working currently is production, resources are
    not stored between phases at this time**

--- Trekgui.py
+++ Trekgui.py
@@ -3,14 +3,19 @@ from tkinter import *
 import tkinter as tk
 from functools import partial
 
-global manufacture 
-global research 
-global culture
-global ascendencyToken 
-global command
+#global variables for resources/structures **currently not implimented**
+manufacture = 0
+research = 0
+culture = 0
+ascendencyToken = 1
+command = 5
+man_building = 1
+res_buidling = 1
+cult_building = 1
+ships = 1
 
 class Window(Frame):
-    #draws window
+    #defines window
     def __init__(self, master=None):
 
         Frame.__init__(self, master)
@@ -19,10 +24,11 @@ class Window(Frame):
         self.resources = {}
         self.init_window()
 
+    # draws window and menu bar
     def init_window(self):
 
         self.master.title("Trek counter")
-        self.pack(fill=BOTH, expand=1)
+        self.pack()
 
         menu = Menu(self.master)
         self.master.config(menu=menu)
@@ -34,7 +40,7 @@ class Window(Frame):
         phase.add_command(label='Start', command=self.startConditions)
         menu.add_cascade(label='Selection', menu=phase)
         
-        
+    # increases all common resoures via add_resource function    
     def productionPhase(self):
         self.place(x=10,y=10)
         self.buttons = {}
@@ -42,16 +48,19 @@ class Window(Frame):
         self.create_button("Research")
         self.create_button("Manufacture")
         
-              
+    # options for structions/ships to build via sub_resource and build_structure
     def buildPhase(self):
         ()
         
+    # drains command values based on action    
     def commandPhase(self):
         ()
-        
+    
+    #**needs working global variables** will reset all values to initial game conditions    
     def startConditions(self):
         ()
 
+    #draws buttons and currently defining temporary variable values *needs changed*
     def create_button(self, name):
         self.resources[name] = 0
         self.buttons[name] = tk.Button(self)
@@ -59,14 +68,22 @@ class Window(Frame):
         self.buttons[name]["command"] = partial(self.add_resource, name)
         self.buttons[name].pack(side="top")
 
+    #incriment resource value
     def add_resource(self, resource):
         self.resources[resource] += 1
         self.buttons[resource]["text"] = resource + " = " + str(self.resources[resource])
-        
+   
+    #subtract combinations of resources, may need multiple functions, consult rules for combinations 
     def build_structure():
         exit()
+        
+    #decrement resource value   
     def sub_resource():
         exit()
+
+    #intend to show read out of all values as text on right column of window   
+    def show_values():
+        ()
 
 root = Tk()
 root.geometry("400x300")
